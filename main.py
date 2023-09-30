af8b29806caded9c3dd359268a05f189eaaf81a8
Max Shen <m4xshen@gmail.com>
2023-09-30 18:21:49 +0800

    feat: detect yaw angle based on the length of rectangle side

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("tile45.png")
+img = cv2.imread("tile_0.png")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 250, 300, apertureSize=3)
 lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
 
 # calculate yaw angle
-thetas = lines[:, 0, 1]
-raw = np.rad2deg(np.abs(lines[:, 0, 1] - np.pi/2))
-print(np.mean(raw[raw < 45]))
+data = lines[:, 0, :]
+
+np.set_printoptions(suppress=True)
+
+theta_mean = np.mean(data[:, 1])
+
+smaller_data = data[data[:, 1] < theta_mean]
+greater_data = data[data[:, 1] >= theta_mean]
+
+sorted_indices = np.argsort(smaller_data[:, 0])
+smaller_data = smaller_data[sorted_indices]
+
+sorted_indices = np.argsort(greater_data[:, 0])
+greater_data = greater_data[sorted_indices]
+
+def get_step(data):
+    tot = 0
+    for x in range(1, len(data)):
+        tot += (data[x, 0] - data[x-1, 0])
+    tot /= len(data)-1
+    return tot
+
+smaller_step = get_step(smaller_data)
+greater_step = get_step(greater_data)
+
+if smaller_step > greater_step:
+    data = smaller_data
+else:
+    data = greater_data
+
+yaw = np.rad2deg(np.mean(data[:, 1])) - 90
+print(yaw)
 
 # draw red lines
 for r_theta in lines:
