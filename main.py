aaed1a6b0e4c4ca1717497ad5464800498f8d596
Max Shen <m4xshen@gmail.com>
2023-09-30 20:30:30 +0800

    refactor: refine code

--- main.py
+++ main.py
@@ -1,62 +1,58 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("tile_0.png")
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 250, 300, apertureSize=3)
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+def calculate_yaw_angle(image_path):
+    image = cv2.imread(image_path)
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(grayscale, 250, 300, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
 
-# calculate yaw angle
-data = lines[:, 0, :]
+    data = lines[:, 0, :]
+    mean_theta = np.mean(data[:, 1])
 
-np.set_printoptions(suppress=True)
+    smaller_data = data[data[:, 1] < mean_theta]
+    greater_data = data[data[:, 1] >= mean_theta]
 
-theta_mean = np.mean(data[:, 1])
+    smaller_data = smaller_data[np.argsort(smaller_data[:, 0])]
+    greater_data = greater_data[np.argsort(greater_data[:, 0])]
 
-smaller_data = data[data[:, 1] < theta_mean]
-greater_data = data[data[:, 1] >= theta_mean]
+    smaller_step = np.mean(np.diff(smaller_data[:, 0]))
+    greater_step = np.mean(np.diff(greater_data[:, 0]))
 
-sorted_indices = np.argsort(smaller_data[:, 0])
-smaller_data = smaller_data[sorted_indices]
+    selected_data = smaller_data if smaller_step > greater_step else greater_data
 
-sorted_indices = np.argsort(greater_data[:, 0])
-greater_data = greater_data[sorted_indices]
+    return np.rad2deg(np.mean(selected_data[:, 1])) - 90
 
-def get_step(data):
-    tot = 0
-    for x in range(1, len(data)):
-        tot += (data[x, 0] - data[x-1, 0])
-    tot /= len(data)-1
-    return tot
 
-smaller_step = get_step(smaller_data)
-greater_step = get_step(greater_data)
+def draw_red_lines(image_path, output_path):
+    image = cv2.imread(image_path)
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(grayscale, 250, 300, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
 
-if smaller_step > greater_step:
-    data = smaller_data
-else:
-    data = greater_data
+    for r_theta in lines:
+        r, theta = r_theta[0]
+        a, b = np.cos(theta), np.sin(theta)
+        x0, y0 = a * r, b * r
+        x1, y1 = int(x0 + 1000 * (-b)), int(y0 + 1000 * (a))
+        x2, y2 = int(x0 - 1000 * (-b)), int(y0 - 1000 * (a))
+        cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-yaw = np.rad2deg(np.mean(data[:, 1])) - 90
-print(yaw)
+    cv2.imwrite(output_path, image)
 
-# draw red lines
-for r_theta in lines:
-    arr = np.array(r_theta[0], dtype=np.float64)
-    r, theta = arr
 
-    a = np.cos(theta)
-    b = np.sin(theta)
+if __name__ == "__main__":
+    print("90:", calculate_yaw_angle("tile_pos90.png"))
+    print("75:", calculate_yaw_angle("tile_pos75.png"))
+    print("45:", calculate_yaw_angle("tile_pos45.png"))
+    print("30:", calculate_yaw_angle("tile_pos30.png"))
+    print("15:", calculate_yaw_angle("tile_pos15.png"))
+    print("0:", calculate_yaw_angle("tile_0.png"))
+    print("-15:", calculate_yaw_angle("tile_neg15.png"))
+    print("-30:", calculate_yaw_angle("tile_neg30.png"))
+    print("-45:", calculate_yaw_angle("tile_neg45.png"))
+    print("-75:", calculate_yaw_angle("tile_neg75.png"))
+    print("-90:", calculate_yaw_angle("tile_neg90.png"))
 
-    x0 = a * r
-    y0 = b * r
-
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
-
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-cv2.imwrite("linesDetected.jpg", img)
+    # draw_red_lines("tile_neg30.png", "linesDetected.jpg")
